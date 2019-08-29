5a21316be29b662e3591a01a6ceb4e441a61d7cc
Egor Antipov <antipovegorka@gmail.com>
2019-08-29 17:20:01 +0200

    Added SetCurrent

--- lib/SPSU/src/SPSU.cpp
+++ lib/SPSU/src/SPSU.cpp
@@ -90,6 +90,37 @@ bool SPSU::setCurrent(float current)
     return EXIT_SUCCESS;
 }
 
+bool SPSU::setCurrent(float current)
+{
+    if(current>8) // Don't burn chips
+        return EXIT_FAILURE;
+
+        int analogValue; // CurrentAdjust value to be written into DigitalPin9 ("CurrentAdjust")
+
+        // I = k * (V_max/255) * CurrentAdjust * (R26/(R25+R26)) / R27
+        // k - adjustment coefficient, there is 11% difference between the expected and measured currents
+        // k actually depends on current. Reffer to https://indico.cern.ch/event/796792/ OSU_HW_Egor slodes
+        // V_max = 5V
+        // CurrentAdjust = 0-255 (int)
+        // R25 = 300k, R26 = 10k, R27 = 0.01
+        // Thus CurrentAdjust = I * R27 / ( (V_max/255) * (R26/(R25+R26)) )
+
+        double V_max = 5.;
+        double r25 = 300000, r26 = 10000, r27 = .01;
+        double k;
+        if (current <= 0.25)                  k = 1;
+        if (current > 0.25 && current <= 0.5) k = 0.97;
+        if (current > 0.7  && current <= 1.0) k = 0.93;
+        if (current > 1.0  && current <= 1.5) k = 0.91;
+        if (current > 1.5  && current <= 2.9) k = 0.9;
+        if (current > 2.9)                    k = 0.89;
+
+        analogValue = (current * r27) / ( (V_max/255) * (r26/(r25+r26)) * k );
+
+        // And now write the analogValue to set requisted current
+        analogWrite(CurrentAdjust, analogValue);
+}
+
 /*********************************************************************
 * Comment
 *********************************************************************/
